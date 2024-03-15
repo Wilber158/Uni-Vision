3d522428458af4bcd056deb286b66ca670ebf728
Wilber Cortez <Wilbercortez2013@gmail.com>
2024-03-14 21:16:45 -0400

    Added Nav Target Script

--- Assets/Scripts/SetNavigationTarget.cs
+++ Assets/Scripts/SetNavigationTarget.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class SetNavigationTarget : MonoBehaviour {
+
+    [SerializeField]
+    private Camera topDownCamera;
+    [SerializeField]
+    private GameObject navTargetObject;
+
+    private UnityEngine.AI.NavMeshPath path;
+    private LineRenderer line;
+
+    private bool lineToggle = false;
+
+    private void Start ()
     {
-        
+        path = new UnityEngine.AI.NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update ()
     {
-        
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            lineToggle = !lineToggle;
+        }
+
+        if (lineToggle)
+        {
+            UnityEngine.AI.NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, UnityEngine.AI.NavMesh.AllAreas, path);
+            line.positionCount = path.corners.Length;
+            line.SetPositions(path.corners);
+            line.enabled = true;
+        }
     }
+
+
+
 }
