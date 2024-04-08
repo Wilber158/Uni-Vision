5367acd0572c4b6abfbb9b6c5cafbf45a058bbb1
Jakel Alonso <alonsojakel60@gmail.com>
2024-04-08 08:18:25 -0400

    login page made
    made login page, sand saves username and password. We can utilize a method to delete the username and password in the future

---
+++ Assets/Scenes/Scripts/saveLoginInfo.cs
@@ -0,0 +1,88 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+public class saveLoginInfo : MonoBehaviour
+{
+    private static readonly byte[] key = Encoding.UTF8.GetBytes("yFYDtjvbkjbrtvkb"); 
+    private static readonly byte[] iv = Encoding.UTF8.GetBytes("UIGKyrdjOMOihvtd");
+
+    // Encrypt a string.
+    public static string EncryptString(string plainText)
+    {
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = key;
+            aesAlg.IV = iv;
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                }
+                return Convert.ToBase64String(msEncrypt.ToArray());
+            }
+        }
+    }
+
+    // Decrypt a string.
+    public static string DecryptString(string cipherText)
+    {
+        var buffer = Convert.FromBase64String(cipherText);
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = key;
+            aesAlg.IV = iv;
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+            using (MemoryStream msDecrypt = new MemoryStream(buffer))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+
+    // Properties for UserId and Password
+    public static string UserId
+    {
+        get => DecryptString(PlayerPrefs.GetString("UserId", ""));
+        set
+        {
+            PlayerPrefs.SetString("UserId", EncryptString(value));
+            PlayerPrefs.Save();
+        }
+    }
+
+    public static string Password
+    {
+        get => DecryptString(PlayerPrefs.GetString("Password", ""));
+        set
+        {
+            PlayerPrefs.SetString("Password", EncryptString(value));
+            PlayerPrefs.Save();
+        }
+    }
+
+    // Check if the login and password exist
+    public static bool AreCredentialsSaved()
+    {
+        //decrypt and log the UserId for debugging purposes.
+        //string testing1 = DecryptString(PlayerPrefs.GetString("UserId"));
+        ..Debug.Log($"UserId: {testing1}");
+        // Check if both UserId and Password have been saved and are not empty
+        return !string.IsNullOrEmpty(PlayerPrefs.GetString("UserId")) && !string.IsNullOrEmpty(PlayerPrefs.GetString("Password"));
+        
+    }
+}
+
--- Assets/Scenes/Scripts/userLogin.cs
+++ Assets/Scenes/Scripts/userLogin.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class userLogin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private TMP_InputField userIdInputField;
+    [SerializeField]
+    private TMP_InputField passwordInputField;
+    [SerializeField]
+    private GameObject loginUI; // Assign the parent GameObject of the login UI in the Unity Inspector
+
+    private void Start()
     {
-        
+        // Check if login details are saved and adjust UI visibility accordingly
+        loginUI.SetActive(!saveLoginInfo.AreCredentialsSaved());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SubmitLoginInformation()
     {
-        
+        // Retrieve input data
+        string userId = userIdInputField.text;
+        string password = passwordInputField.text;
+
+        // Securely save the data using SecureDataManager
+        saveLoginInfo.UserId = userId;
+        saveLoginInfo.Password = password;
+
+        loginUI.SetActive(false);
+
+        Debug.Log("Login information submitted and saved securely.");
     }
 }
