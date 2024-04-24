8d6646d563e034c0091308e04bac6bd5b85fb208
Wilber Cortez <Wilbercortez2013@gmail.com>
2024-04-23 23:34:48 -0400

    Fixed SQL error
    On MAC OS, SQLite is native, therefore adding a SQLite Script to the script folder will stop the error

--- Assets/Scenes/Scripts/scrapper.cs
+++ Assets/Scenes/Scripts/scrapper.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.IO;
 using System;
-using Unity.VisualScripting.Dependencies.Sqlite;
+using SQLite;
 
 public class SeleniumExample : MonoBehaviour
 {
